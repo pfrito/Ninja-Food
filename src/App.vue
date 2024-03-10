8be311921f35d644bffab8131a9cf701539ef84e
pfrito <carlos.rolando.morell@gmail.com>
2024-03-10 19:32:30 -0400

    changed display of items on md screen

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 
 <template>
   <body class="text-gray-600 font-body">
-    <div>
+    <div class="grid md:grid-cols-3">
       <!-- content wrapper -->
-      <div>
+      <div class="md:col-span-1">
         <nav>
           <div>
             <h1 class="font-bold uppercase p-4 border-b border-gray-100">
@@ -32,7 +32,7 @@
       </div>
       <!-- end nav -->
 
-      <main class="px-16 py-6 bg-gray-100">
+      <main class="px-16 py-6 bg-gray-100 md:col-span-2">
         <div class="flex justify-center md:justify-end">
           <a href="#" class="text-primary">Login</a>
           <a href="#" class="text-primary ml-2">Sign up</a>
@@ -47,15 +47,37 @@
           <h4 class="font-bold mt-12 pb-2 border-b border-gray-200">
             Latest Recipes
           </h4>
-          <div class="mt-8">
+          <div class="mt-8 grid lg:grid-cols-3 gap-10">
             <!-- cards go here -->
-            <div class="bg-white rounded overflow-hidden shadow-md relative">
-              <img src="./assets/stew.jpg" alt="" class="w-full h-32 sm:h-48 object-cover"/>
+            <div class="card">
+              <img src="./assets/stew.jpg" alt="stew" class="w-full h-32 sm:h-48 object-cover"/>
               <div class="m-4">
                 <span class="font-bold ">5 Bean Chilli Stew</span>
                 <span class="block text-gray-500 text-sm">Recipe by Mario</span>
               </div>
-              <div class="bg-secondary-100 text-secondary-200 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2">
+              <div class="batch">
+                <span>25 mins</span>
+              </div>
+            </div>
+
+            <div class="card">
+              <img src="./assets/noodles.jpg" alt="noodles" class="w-full h-32 sm:h-48 object-cover"/>
+              <div class="m-4">
+                <span class="font-bold ">Veg Noodles</span>
+                <span class="block text-gray-500 text-sm">Recipe by Mario</span>
+              </div>
+              <div class="batch">
+                <span>25 mins</span>
+              </div>
+            </div>
+
+            <div class="card">
+              <img src="./assets/curry.jpg" alt="curry" class="w-full h-32 sm:h-48 object-cover"/>
+              <div class="m-4">
+                <span class="font-bold ">Tofu Curry</span>
+                <span class="block text-gray-500 text-sm">Recipe by Mario</span>
+              </div>
+              <div class="batch">
                 <span>25 mins</span>
               </div>
             </div>
